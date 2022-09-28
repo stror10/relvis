97c3456e224768a9656457b78767f423f8ccb49f
stror10 <str.or10@gmail.com>
2022-09-28 20:55:27 +0300

    Big changes to relvise

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import * as flsFunctions from "./modules/functions.js";
+
+//import * as burger from "./burger.js";
+
+flsFunctions.isWebp();
+
+//import Swiper, { Navigation, Pagination } from 'swiper';
+
+//const swiper = new Swiper();
+
